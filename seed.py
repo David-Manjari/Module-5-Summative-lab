224e0470dec84515d688780e7f441d64d7841dfd
Manjari <manjari@Manjari.localdomain>
2026-09-10 23:17:31 +0300

    Created the context in seed file

--- seed.py
+++ seed.py
@@ -1,2 +1,5 @@
 # Create Initial Data for the Database to check if the database is working properly
+from app import app
+from models import User, Workout, Session, Exercise, db
 
+with app.app_context():
