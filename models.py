84e07bd6d1664fcfcce0165e0cf713598b4ef981
Manjari <manjari@Manjari.localdomain>
2026-09-11 06:16:57 +0300

    Fixed syntax in my relationships

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 # Import the libraries for this project 
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, ForeignKey, create_engine,sessionmaker
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, MetaData
+
 
 
 convention = {
@@ -15,14 +16,13 @@ db = SQLAlchemy(metadata=metadata)
 
 user_workout = db.Table('user_workout',
     db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-    db.Column('workout_id', db.Integer, db.ForeignKey('workouts.id'), primary_key=True)
+    db.Column('workout_id', db.Integer, db.ForeignKey('workouts.id'), primary_key=True),
     db.Column('session_id', db.Integer, db.ForeignKey('sessions.id'), primary_key=True)
 )
 class BaseModel:
     """Base model class that other models will inherit from."""
     id = Column(Integer, primary_key=True)
 
-session = sessionmaker(bind=engine)()
 
 
 # My models will be defined here
@@ -48,15 +48,16 @@ class Workout(BaseModel, db.Model):
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
 
     #  create the relationship between the Workout and Exercise models
-
+    user = db.relationship('User', back_populates='workouts')
     sessions = db.relationship('Session', back_populates='workout')
     exercises = db.relationship('Exercise', back_populates='workout')
 
     def __repr__(self):
-        return f'<Workout {self.name}>''
+        return f'<Workout {self.name}>'
 
 class Session(BaseModel, db.Model):
     __tablename__ = 'sessions'
+    name = Column(String(80), unique=True, nullable=False)
     date = Column(String(80), nullable=False)
     duration = Column(Integer, nullable=False)
     calories_burned = Column(Integer, nullable=False)
@@ -65,7 +66,9 @@ class Session(BaseModel, db.Model):
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
 
     # create the relationship between the Session User, workout and Exercise models
-    exercise = db.relationship('Exercise', back_populates='sessions')
+    user = db.relationship('User', back_populates='sessions')
+    workout = db.relationship('Workout', back_populates='sessions')
+    exercises = db.relationship('Exercise', back_populates='sessions')
 
     def __repr__(self):
         return f'<Session {self.date}>'
@@ -76,10 +79,15 @@ class Exercise(BaseModel, db.Model):
     description = Column(String(200), nullable=False)
     sets = Column(Integer, nullable=False)
     reps = Column(Integer, nullable=False)
-
+    steps = Column(Integer, nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     workout_id = Column(Integer, ForeignKey('workouts.id'), nullable=False)
     session_id = Column(Integer, ForeignKey('sessions.id'), nullable=False)
+
+    # create the relationship between the Exercise User, workout and session models
+    user = db.relationship('User', back_populates='exercises')
+    workout = db.relationship('Workout', back_populates='exercises')    
+    sessions = db.relationship('Session', back_populates='exercises')
 
     def __repr__(self):
         return f'<Exercise {self.name}>'
