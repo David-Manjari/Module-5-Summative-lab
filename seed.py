9e9a73785a9961ad0655c8e39113793d5f6991a0
Manjari <manjari@Manjari.localdomain>
2026-09-11 06:18:08 +0300

    Added missing seed data to my tables

--- seed.py
+++ seed.py
@@ -7,39 +7,45 @@ with app.app_context():
     db.create_all()
 
     # Create initial data
-    user1 = User(username='john_doe', 
-                email='john@example.com', 
+    user1 = User(username='John Muchiri', 
+                email='john@thisapp.com', 
                 password='password')
-    user2 = User(username='jane_doe', 
-                email = "jane@example.com", 
+    user2 = User(username='Ruth Onyango', 
+                email = "ruth@thisapp.com", 
                 password = "password")
-    workout1 = Workout(name='Workout 1',
-                        description='This is the first workout',
+    workout1 = Workout(name='Shoulders and Arms',
+                        description='This is a workout to get bigger, more defined and powerful shoulders and arms',
                         date='2023-01-01',
                         user_id=1)
-    workout2 = Workout(name='Workout 2',
-                        description='This is the second workout',
+    workout2 = Workout(name='Chest and Triceps',
+                        description='This is a workout to get bigger, more defined and powerful chest and triceps',
                         date='2023-01-02',
                         user_id=2)
-    session1 = Session(date='2023-01-01',
+    session1 = Session(name='MorningSession',    
+                        date='2023-01-01',
                         duration=60,
                         calories_burned=500,
-                        workout_id=1)
-    session2 = Session(date='2023-01-02',
+                        workout_id=1,
+                        user_id=1)
+    session2 = Session(name='Evening Session',
+                        date='2023-01-02',
                         duration=45,
                         calories_burned=400,
-                        workout_id=2)   
-    exercise1 = Exercise(name='Exercise 1',
+                        workout_id=2,
+                        user_id=2)   
+    exercise1 = Exercise(name='Barbell Shoulder Press',
                         description='This is the first exercise',
                         sets=3,
                         reps=10,
+                        steps=5,
                         user_id=1,
                         workout_id=1,
                         session_id=1)
-    exercise2 = Exercise(name='Exercise 2',
+    exercise2 = Exercise(name='Dumbbell Lateral Raise',
                         description='This is the second exercise',
                         sets=4,
                         reps=12,
+                        steps=6,
                         user_id=2,
                         workout_id=2,
                         session_id=2)
