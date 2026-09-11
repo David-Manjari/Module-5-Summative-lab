39f99afe7f3cd86edd1558f0a9fa9f2ed5932427
Manjari <manjari@Manjari.localdomain>
2026-09-11 06:38:06 +0300

    Modified my association tables and fixed some syntax errors

--- models.py
+++ models.py
@@ -14,10 +14,14 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(metadata=metadata)
 
-user_workout = db.Table('user_workout',
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+workout_exercise = db.Table('workout_exercise',
     db.Column('workout_id', db.Integer, db.ForeignKey('workouts.id'), primary_key=True),
-    db.Column('session_id', db.Integer, db.ForeignKey('sessions.id'), primary_key=True)
+    db.Column('exercise_id', db.Integer, db.ForeignKey('exercises.id'), primary_key=True)
+)
+
+session_exercise = db.Table('session_exercise',
+    db.Column('session_id', db.Integer, db.ForeignKey('sessions.id'), primary_key=True),
+    db.Column('exercise_id', db.Integer, db.ForeignKey('exercises.id'), primary_key=True)
 )
 class BaseModel:
     """Base model class that other models will inherit from."""
@@ -50,7 +54,7 @@ class Workout(BaseModel, db.Model):
     #  create the relationship between the Workout and Exercise models
     user = db.relationship('User', back_populates='workouts')
     sessions = db.relationship('Session', back_populates='workout')
-    exercises = db.relationship('Exercise', back_populates='workout')
+    exercises = db.relationship('Exercise',secondary=workout_exercise, back_populates='workout')
 
     def __repr__(self):
         return f'<Workout {self.name}>'
@@ -68,7 +72,7 @@ class Session(BaseModel, db.Model):
     # create the relationship between the Session User, workout and Exercise models
     user = db.relationship('User', back_populates='sessions')
     workout = db.relationship('Workout', back_populates='sessions')
-    exercises = db.relationship('Exercise', back_populates='sessions')
+    exercises = db.relationship('Exercise',secondary=session_exercise, back_populates='sessions')
 
     def __repr__(self):
         return f'<Session {self.date}>'
